7c749d54345b10ae94db74e1209f83f394b97477
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-20 22:42:24 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,7 +2,8 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       
-      
+      t.integer :passenger_id
+      t.integer :taxi_id
       
       t.timestamps null: false
     end
